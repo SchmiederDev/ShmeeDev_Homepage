729f1d0f5ae27f946a2e53ddb44fa34e051d11a2
katrin <katrin.kampfrath@gmail.com>
2026-03-07 10:03:27 +0100

    Add 11ty-navigation and basic desktop menu

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { imageShortcode } from "./src/_11ty/imageShortcode.js";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPassthroughCopy("./src/assets/fonts");
   eleventyConfig.addPassthroughCopy("./src/assets/images");
@@ -11,15 +13,6 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets/vendor/**/*.css");
 
   eleventyConfig.addLayoutAlias("base", "layout/base");
-
-  eleventyConfig.addCollection("pages", function (collectionsApi) {
-    return collectionsApi
-      .getAll()
-      .filter(function (item) {
-        return "listed" in item.data;
-      })
-      .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
-  });
 
   imageShortcode(eleventyConfig);
 }
--- src/assets/main.js
+++ src/assets/main.js
@@ -1,8 +1,50 @@
-import Splide from '@splidejs/splide';
-// import '@splidejs/splide/css';
+const bool2str = b => b ? 'true' : 'false';
 
-new Splide( '.splide' ).mount();
+const toggleAriaExpanded = (el, state = undefined) => {
+    ((el) => el.setAttribute('aria-expanded',
+        state !== undefined
+            ? state
+            : bool2str(el.getAttribute('aria-expanded') !== 'true'))
+    )(el);
+};
 
+const linkToButton = (el, ariaExpand = true, hasPopup = false) => {
+    const button = document.createElement('BUTTON');
+    button.innerHTML = el.innerHTML;
+    for (let i = 0; i < el.attributes.length; i++) {
+        const attrib = el.attributes[i];
+        if (attrib.name === 'href' || attrib.name === 'target') {
+            continue;
+        }
+        button.setAttribute(attrib.name, attrib.value);
+        if (ariaExpand) {
+            button.setAttribute('aria-expanded', 'false');
+        }
+        if (hasPopup) {
+            button.setAttribute('aria-haspopup', 'true');
+        }
+    }
+    el.parentNode.insertBefore(button, el);
+    el.parentNode.removeChild(el);
+    return button;
+};
 
+const handleMenuLinkClick = (ev) => {
+  toggleAriaExpanded(ev.target);
+};
 
-console.log('hello my dear'); // eslint-disable-line
+const initMainNav = () => {
+  const buttonlinks = document.querySelectorAll('.main-nav__link:has(+ .main-nav__sub-list)');
+  console.log(buttonlinks); // eslint-disable-line
+
+  buttonlinks.forEach(link => {
+    link = linkToButton(link);
+    link.removeAttribute('aria-current');
+    link.addEventListener('click', handleMenuLinkClick);
+  })
+};
+
+// init on desktop
+// add resize
+// add focusout
+document.addEventListener( 'DOMContentLoaded', initMainNav);
