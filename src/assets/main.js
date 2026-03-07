5e213607cd68264d7aa9bc5cdd45521906dab38a
katrin <katrin.kampfrath@gmail.com>
2026-03-07 13:07:16 +0100

    Add mobile main nav handling

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,3 +1,23 @@
+/*
+======================================================
+Utils
+*/
+
+const forAllIn = (el) => (selector, fn) => Array.prototype.forEach.call(el.querySelectorAll(selector), fn);
+const forAll = forAllIn(document);
+
+const isMobile = () => (e => e && e.offsetParent !== null)(document.querySelector('.main-nav__mobile-toggle'));
+
+const debounce = (fn, delay = 50) => {
+    let to;
+    return function() {
+        if (to) {
+            clearTimeout(to);
+        }
+        to = setTimeout(() => fn(arguments), delay);
+    };
+};
+
 const bool2str = b => b ? 'true' : 'false';
 
 const toggleAriaExpanded = (el, state = undefined) => {
@@ -8,7 +28,7 @@ const toggleAriaExpanded = (el, state = undefined) => {
     )(el);
 };
 
-const linkToButton = (el, ariaExpand = true, hasPopup = false) => {
+const linkToButton = (el, ariaExpand = true) => {
     const button = document.createElement('BUTTON');
     button.innerHTML = el.innerHTML;
     for (let i = 0; i < el.attributes.length; i++) {
@@ -20,31 +40,75 @@ const linkToButton = (el, ariaExpand = true, hasPopup = false) => {
         if (ariaExpand) {
             button.setAttribute('aria-expanded', 'false');
         }
-        if (hasPopup) {
-            button.setAttribute('aria-haspopup', 'true');
-        }
     }
     el.parentNode.insertBefore(button, el);
     el.parentNode.removeChild(el);
     return button;
 };
 
+/*
+======================================================
+Main Nav handling
+*/
+
 const handleMenuLinkClick = (ev) => {
   toggleAriaExpanded(ev.target);
 };
 
 const initMainNav = () => {
-  const buttonlinks = document.querySelectorAll('.main-nav__link:has(+ .main-nav__sub-list)');
-  console.log(buttonlinks); // eslint-disable-line
+  if (isMobile()) {
+    document.querySelector('.main-nav__mobile-toggle').addEventListener('click', handleMenuLinkClick);
+  }
+  if (!isMobile()) {
+    forAll('.main-nav__link:has(+ .main-nav__sub-list)',
+      link => {
+        link = linkToButton(link);
+        link.removeAttribute('aria-current');
+        link.addEventListener('click', handleMenuLinkClick);
+      }
+    );
+  }
+};
 
-  buttonlinks.forEach(link => {
-    link = linkToButton(link);
-    link.removeAttribute('aria-current');
-    link.addEventListener('click', handleMenuLinkClick);
-  })
+/*
+======================================================
+Resize handler
+*/
+
+let header, currentHeaderWidth;
+const headerBreakpoint = 1380;
+
+const isChangingBreakpoint = (newWidth) => {
+  console.log(newWidth); // eslint-disable-line
+    const prevWidth = currentHeaderWidth;
+    currentHeaderWidth = newWidth;
+    if ((prevWidth > headerBreakpoint) && (newWidth > headerBreakpoint)) {
+        return false;
+    }
+    if ((prevWidth < headerBreakpoint) && (newWidth < headerBreakpoint)) {
+        return false;
+    }
+    return true;
+};
+
+const resetAllMenus = () => {
+    if (isChangingBreakpoint(header.clientWidth)) {
+        window.location.reload();
+    }
+};
+
+const initResizeHandler = () => {
+    header = document.querySelector('.page-header');
+    currentHeaderWidth = header.clientWidth;
+    window.addEventListener('resize', debounce(resetAllMenus, 100, {
+        'leading': false,
+        'trailing': true,
+    }));
 };
 
 // init on desktop
-// add resize
 // add focusout
-document.addEventListener( 'DOMContentLoaded', initMainNav);
+document.addEventListener( 'DOMContentLoaded', function() {
+  initResizeHandler();
+  initMainNav();
+});
