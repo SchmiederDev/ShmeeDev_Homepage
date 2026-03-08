a8e285adea7f445f9f8c4626e5820da058e468b3
katrin <katrin.kampfrath@gmail.com>
2026-03-08 10:29:25 +0100

    Add favicon, opengraph img and stuff

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets/fonts");
   eleventyConfig.addPassthroughCopy("./src/assets/images");
   eleventyConfig.addPassthroughCopy("./src/assets/main.js");
+  eleventyConfig.addPassthroughCopy({"./src/assets/favicons": "/"});
   eleventyConfig.addPassthroughCopy("./src/assets/vendor/**/*.js");
   eleventyConfig.addPassthroughCopy("./src/assets/vendor/**/*.css");
 
